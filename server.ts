505701f9752fa046f580d15953bdd7e98775a575
satyasootar <satya.sootar06@gmail.com>
2025-12-11 01:54:01 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,7 +1,8 @@
 import app from './src/app.ts'
+import  {config}  from './src/config/config.ts';
 
 const startServer =()=>{
-    const port = process.env.PORT || 3000
+    const port = config.port || 3000
 
     app.listen(port, ()=>{
         console.log("The server is listning at port:", port);
